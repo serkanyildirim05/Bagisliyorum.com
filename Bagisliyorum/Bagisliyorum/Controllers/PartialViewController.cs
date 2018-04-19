dce43059c3044e0b004ba8fba371a4377260f8e5
Serkan <srkn.yldrmfb@gmail.com>
2018-04-19 14:47:28 +0300

    ddd

---
+++ Bagisliyorum/Bagisliyorum/Controllers/PartialViewController.cs
@@ -0,0 +1,19 @@
+﻿using Bagisliyorum.Repository.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bagisliyorum.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        HastalarConcrete hc = new HastalarConcrete();
+        [ChildActionOnly]
+        public ActionResult HastaGetir()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bagisliyorum/Bagisliyorum/Repository/Concrete/HastalarConcrete.cs
@@ -0,0 +1,19 @@
+﻿using Bagisliyorum.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bagisliyorum.Repository.Concrete
+{
+    public class HastalarConcrete
+    {
+        public Hastalar HastaGetir()
+        {
+            using (BagislarDBEntities db = new BagislarDBEntities())
+            {
+                return db.Hastalars.OrderByDescending(p => p.Hasta_No).FirstOrDefault();
+            }
+        }
+    }
+}
